73ec092a4fd7430129b1cb20cf539bae2e30afa0
Samuelwps <samuelrip230@gmail.com>
2022-06-09 20:33:31 -0300

    first commit

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,36 +1,7 @@
-import { Star, Clock } from 'react-feather';
-
-import '../styles/movie-card.scss';
-
-interface MovieCardProps {
-  title: string;
-  poster: string;
-  rating: string;
-  runtime: string;
-}
-
-export function MovieCard(props: MovieCardProps) {
+function MovieCard() {
   return (
-    <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
-
-      <div>
-        <div className="movie-info">
-          <span>{props.title}</span>
-          <div className="meta">
-            <div>
-              <Star /> {props.rating}
-            </div>
-
-            <div>
-              <Clock /> {props.runtime}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <h2>hey</h2>
   )
 }
+
+export default MovieCard
