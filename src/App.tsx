7539ade24b87decd81cb71df4655b8d7c07f660d
Samuelwps <samuelrip230@gmail.com>
2022-06-10 20:21:48 -0300

    Updathing in the CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { api } from "./services/api"
 
 import  Button from "./components/Button"
 
+import MovieCard from "./components/MovieCard";
+
 
 interface GenreResponseProps {
   id: number;
@@ -11,18 +13,52 @@ interface GenreResponseProps {
   title: string;
 }
 
+interface MovieProps{
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
 function App() {
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
   const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+
+  
 
 
   useEffect(() => {
     api.get<GenreResponseProps[]>("genres").then(response => {
       setGenres(response.data)
+      console.log("setGenres")
       console.log(response.data)
     })
     }, [])
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+      console.log("setMovies")
+      console.log(response.data)
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+      console.log("setSelectedGenre")
+      console.log(response.data)
+    })
+  }, [selectedGenreId]);
     
   function handleClickButton(id: number) {
     setSelectedGenreId(id);
@@ -46,8 +82,6 @@ function App() {
             })
           }
         </div>
-
-
       </nav>
     </div>
     
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,38 @@
-import  Icon  from "./Icon"
-
 import { ButtonHTMLAttributes } from "react"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   title: string;
-  iconName: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
-  selected: boolean;
 }
 
-function Button({ iconName, title, selected, ...reset} : ButtonProps) {
-  return (
-    <button type="button" {...(selected && { className: "selected"})} {...reset}>
-
-      <Icon name={iconName} color={selected ? "#FAE800" : "#FBFBFB"}/>
-      {title}
-    </button>
+function Button(props : ButtonProps){
+  return(
+    <h1>{props.title}</h1>
+    
   );
 }
 
 export default Button
+
+
+
+
+
+
+
+
+// import  Icon  from "./Icon"
+
+// import { ButtonHTMLAttributes } from "react"
+
+// 
+
+// function Button({ iconName, title, selected, ...reset} : ButtonProps) {
+//   return (
+//     <button type="button" {...(selected && { className: "selected"})} {...reset}>
+//       <Icon name={iconName} color={selected ? "#FAE800" : "#FBFBFB"}/>
+//       {title}
+//     </button>
+//   );
+// }
+
+// export default Button
