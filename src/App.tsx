cd08b6f02654f9a493721e18e7e5e30e447c9b9b
Samuelwps <samuelrip230@gmail.com>
2022-06-10 18:48:12 -0300

    Upadating the CSS in the Project-Movie

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 
 import { api } from "./services/api"
 
+import  Button from "./components/Button"
+
+
 interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
@@ -10,6 +13,9 @@ interface GenreResponseProps {
 
 function App() {
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
 
   useEffect(() => {
     api.get<GenreResponseProps[]>("genres").then(response => {
@@ -18,20 +24,24 @@ function App() {
     })
     }, [])
     
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
 
   return (
     <div>
-      <nav className="sisdebar">
+      <nav className="sidebar">
         <span>Watch <p>Me</p></span>
 
         <div className="buttons-container">
           {
             genres.map(genre => {
-              <     
+              <Button     
                 key={String(genre.id)}
                 title={genre.title}
                 iconName={genre.name}
-                onClick{() => handleClickButton }
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
               />
             })
           }
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ import { render } from 'react-dom'
 
 import  App  from './App'
 
+import "./CSS/style.scss"
+
 render(<App />, document.getElementById('root'))
